91b70ffecf89913700395c70226b1e92b542872b
Muhammadahsan178 <65031295+Muhammadahsan178@users.noreply.github.com>
2020-06-14 16:42:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,233 @@
+
+// CHAPTER NO 1
+
+//  TASK #1
+
+// alert("Welcome To Solex.pk");
+
+//  TASK #2
+
+// alert("Error! Please Enter a valid password.");
+
+
+//  TASK #3
+
+// alert("Welcome to JS Land... \n Happy Coding!");
+
+
+
+//  TASK #4
+// alert("Welcome to JS Land...")
+// alert("  Happy Coding!");
+
+
+//  TASK #5
+
+
+// console.log("Hello... I can run JS through my web browser console");
+
+
+// *******************
+
+// chapter NO 2
+// TASK #1
+
+// var a="UserName:";
+// var b=" " + "M_Ahsan";
+// var c=a+b;
+// alert(c);
+
+
+// task #2
+
+// var my= "Ahsan";
+// var Name="Asif";
+// var c=my+" "+Name;
+// alert(c)
+
+// task #3
+
+// var a="Hello";
+// var b="World";
+// var c=a+b;
+// alert(c);
+
+// task#4
+
+// var a = "Ahsan";
+// var b = "Asif";
+// var c = a+ " " +b;
+// alert(c)
+
+// var d = 20;
+// var e = "years Old";
+// var f = d+" "+e;
+// alert(f)
+
+// var g = "Certified Mobile";
+// var h = "Application Development";
+// var i= g + " " + h;
+// alert(i)
+
+
+// Task #5
+
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+// Task #6
+
+// var a="E-mail:";
+// var b="muhammadahsan178@gmail.com"
+// var c= a + " " + b;
+// alert(c)
+
+// task#7
+
+// var book="A Smarter way to learn Javascript";
+// alert ("I am trying to learn from the book" + " " + book)
+
+// task#8
+
+// document.write("yah! I can write HTML content through Javascript.");
+
+// task#9
+
+// var a="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(a)
+// console.log(a)
+
+
+// *******************
+// CHAPTER #3
+
+// TASK#1
+
+// var age= 20;
+// var years=" Years old";
+// var c=age+years;
+// alert("I Am"+" "+ c)
+
+//       Task#2
+// var n= 14;
+// alert("You have visited this site" +" "+ n + " " + "Times");
+
+
+
+//     Task#3
+// var a="My Birth Year is";
+// var b=1990;
+// var c= a + " " + b +"\n"+ "Data type Of my declared variable is number" ;
+// alert(c)
+
+
+//        // Task#4
+//        var Name="Mr.Ahsan";
+//        var title="XYZ Clothing Store";
+//        var Quantity=" 5 T-Shirt(s)";
+//        var a= Name +" "+"ordered"+ Quantity+" on" +" " + title;
+//       document.write(a);
+
+
+
+
+                 // *******************
+                  // CHAPTER #4
+
+       // TASK#1
+
+// var Name = "Mr.Ahsan";
+// var title = "XYZ Clothing Store";
+// var Quantity = " 5 T-Shirt(s)";
+// var a= Name +" "+"ordered"+ Quantity+" on" +" " + title;
+// alert(a)
+
+
+           // Task#2
+ 
+       //     legal Names
+
+//        var a= "A variable name can't contain any spaces,";
+// var b= "A variable name can contain only letters, numbers, dollar signs, and underscores";
+// var c= a + " " + b;
+// document.write(c); 
+
+     // il-leagal name
+
+
+
+
+//      task#3
+
+// var a= " a) A heading stating Rules for naming JS variables";
+// var b= " b) Variable names can only contain" + "," + " "+ " numbers," +" "+  "$," +" "+ "_."+ " "+  "For example $my_1stVariable" ;
+// var c= "c) Variables must begin with a letter, $, _." + "\n" +"For example $name, _name or name";
+// var d= "d) Variable names are case sensitive.";
+// var e= "e) Variable names should not be JS keywords.";
+// var f= a+b+c+d+e;
+
+// a + "\n" + b+ "\n"+ c+ "\n"+d+ "\n"+e+ "\n";
+// document.write(f);
+
+
+
+                 // *******************
+                  // CHAPTER #5
+
+       // TASK#1
+
+// var a= 5;
+// var b= 6;
+// var c= a +b;
+// document.write("Sum of 5 and 6 is"+ " "+c);
+
+//     TASK#2
+                     //    (multiplication process)
+// var a= 5;
+// var b= 6;                   
+// var c= a*b;
+// document.write(c);
+
+
+                     //    (division Process)
+// var a= 30;
+// var b= 6;                   
+// var c= a/b;
+// document.write(c);
+
+
+              //   modulus Process
+
+// var a= 49;
+// var b= 7;                   
+// var c= a%b;
+// document.write(c);
+
+
+              // Task #3
+
+//  var a= 1;
+//  var b=5;
+//  var c=a+b;
+//  document.write("<br> value after increment is:"+  c +"</br>"  );
+//  var d=c+7;
+//  document.write("<br> \nvalue after addition is:" + d +"</br>" );
+// var e=d-1;
+// document.write("<br> \nvalue after decrement is:"+ e +"</br>" );
+// var f=e/3;
+// document.write("<br> \nThe remainder is:"+ f +"</br>" );
+
+              // Task#4
+// var Quantity=5;
+// var price=600;
+// var c=price*Quantity;
+// document.write("Total cost to buy 5 tickets to a movie is" +" " + c +"PKR" );
+
+              //       TASK#5
+
+       // for(var i=1; i<=10; i++){
+       //        document.write("4" + "x" + i + "=" + 4*i + "<br>" )
+       // }
+
+       // Task#6
+
+       var a= 25
